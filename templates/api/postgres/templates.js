232e98eb1f66baa474266501d7eccc0d06e7dda7
Leonardozn <mirapertuz@gmail.com>
2022-03-05 13:14:09 -0500

    Interfaces file and commands deleted

--- templates/api/postgres/templates.js
+++ templates/api/postgres/templates.js
@@ -16,15 +16,6 @@ const sessionTemplate = require('./session-template')
 const virtualsTemplate = require('./virtuals-template')
 const authControllerTemplate = require('./auth-controller-template')
 const authRouteTemplate = require('./auth-route-template')
-const vueAppTemplate = require('./vueApp-template')
-const vueDataTableTemplate = require('./vueDataTable-template')
-const vueRouterTemplate = require('./vueRouter-template')
-const vueHomeTemplate = require('./vueHome-template')
-const vueHomeViewTemplate = require('./vueHomeView-template')
-const vueModelViewTemplate = require('./vueModelView-template')
-const vueStoreTemplate = require('./vueStore-template')
-const vueLoginTemplate = require('./vueLogin-template')
-const vueLoginViewTemplate = require('./vueLoginView-template')
 
 module.exports = {
     indexTemplate,
@@ -44,14 +35,5 @@ module.exports = {
     sessionTemplate,
     virtualsTemplate,
     authControllerTemplate,
-    authRouteTemplate,
-    vueAppTemplate,
-    vueDataTableTemplate,
-    vueRouterTemplate,
-    vueHomeTemplate,
-    vueHomeViewTemplate,
-    vueModelViewTemplate,
-    vueStoreTemplate,
-    vueLoginTemplate,
-    vueLoginViewTemplate
+    authRouteTemplate
 }
