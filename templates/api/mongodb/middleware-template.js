c879aa113c67220c9d6e401d6c550b127896e4cc
Leonardozn <mirapertuz@gmail.com>
2022-11-13 16:49:05 -0500

    complete changes

--- templates/api/mongodb/middleware-template.js
+++ templates/api/mongodb/middleware-template.js
@@ -1,17 +1,32 @@
-function content(model) {
-    let template = `const Joi = require('joi')
-const errorMessages = require('../helpers/errorMessages')
+function buildArrayStructure(field, spaces) {
+    let jump = false
+    if (field.contentType == 'Object') jump = true
 
-function validationSchema(req, res, next) {
-    const schema = Joi.object({\n`
+    let template = `Joi.array().items(${jump ? `\n${spaces}\t` : ''}`
 
-    Object.keys(model.fields).forEach((key, i) => {
-        const field = model.fields[key]
-        template += `\t\t${key}: `
+    if (field.contentType == 'String' || field.contentType == 'ObjectId') template += `Joi.string()`
+    if (field.contentType == 'Number') template += `Joi.number()`
+    if (field.contentType == 'Date') template += `Joi.date()`
+    if (field.contentType == 'Object') template += buildObjectStructure(field.structure, `${spaces}\t`)
+
+    template += `${jump ? `\n${spaces.substr(0, spaces.length-1)}` : ''})`
+    
+    return template
+}
+
+function buildObjectStructure(fields, spaces) {
+    let template = `Joi.object({\n`
+
+    Object.keys(fields).forEach((key, i) => {
+        const field = fields[key]
+        template += `${spaces}${key}: `
         
         if (field.type == 'String' || field.type == 'ObjectId') template += `Joi.string()`
         if (field.type == 'Number') template += `Joi.number()`
         if (field.type == 'Date') template += `Joi.date()`
+        if (field.type == 'Object') template += buildObjectStructure(field.structure, `${spaces}\t`)
+        if (field.type == 'Array') template += buildArrayStructure(field, `${spaces}\t`)
+
         if (field.unique || field.required) {
             template += `.required()`
         } else {
@@ -19,12 +34,22 @@ function validationSchema(req, res, next) {
             if (field.type == 'Number') template += `.allow(null)`
         }
 
-        if (i < Object.keys(model.fields).length - 1) template += `,\n`
+        if (i < Object.keys(fields).length - 1) template += `,\n`
     })
-
-    template += `\n\t})
     
-    try {
+    template += `\n${spaces.substr(0, spaces.length-1)}})`
+
+    return template
+}
+
+function content(model) {
+    let template = `const Joi = require('joi')
+const errorMessages = require('../helpers/errorMessages')
+
+function validationSchema(req, res, next) {
+    const schema = ${buildObjectStructure(model.fields, '\t\t')}`
+    
+    template += `\n\n\ttry {
 		if (Array.isArray(req.body)) {
 			for (let item of req.body) {
 				const { error, value } = schema.validate(item)
